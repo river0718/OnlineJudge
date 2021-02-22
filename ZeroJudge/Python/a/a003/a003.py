470334e71c1634b1c7d82a741a1b12410b717d27
river <h90093000054@gmail.com>
2021-02-22 20:27:44 +0800

    upload a003.py

---
+++ ZeroJudge/Python/a/a003/a003.py
@@ -0,0 +1,14 @@
+while True:
+    try:
+        m, d = map(int, input().split())
+        s = (m * 2 + d) % 3
+
+        if s == 0:
+            print("普通")
+        elif s == 1:
+            print("吉")
+        else:
+            print("大吉")
+
+    except:
+        break
