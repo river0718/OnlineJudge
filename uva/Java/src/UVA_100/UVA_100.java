4a63fd5a431aa686eb099061f7120a22c250718e
river <h90093000054@gmail.com>
2021-02-24 21:19:07 +0800

    upload uva_100.java

---
+++ uva/Java/src/UVA_100/UVA_100.java
@@ -0,0 +1,47 @@
+package UVA_100;
+import java.util.Scanner;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
+public class UVA_100 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        while (input.hasNext()) {
+            int number1, number2, max_length = 0, temp = 0;
+
+            number1 = input.nextInt();
+            number2 = input.nextInt();
+
+            for (int i = min(number1, number2); i <= max(number1, number2); i++) {
+                temp = circle(i);
+
+                if (temp > max_length) {
+                    max_length = temp;
+                }
+            }
+
+            System.out.println(number1 + " " + number2 + " " + max_length);
+        }
+    }
+
+    public static int circle(int i) {
+        int times = 1;
+
+        while (i != 1) {
+            if (i % 2 != 0) {
+                i = (3 * i + 1) / 2;
+
+                times = times + 2;
+            }
+            else {
+                i = i / 2;
+
+                times++;
+            }
+        }
+
+        return times;
+    }
+}
